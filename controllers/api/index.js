9e3ded185bbede33c67ab5eab38eb6fa01edc44b
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-12-03 15:45:16 -0600

    starting Owner Home Page and have team names and current wins/losses records displaying from the api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const ownerRoutes = require('./ownerRoutes');
 const teamRoutes = require('./teamRoutes');
 const teamNames = require('./teamNames');
+const teamsData = require('./ownerHomePage');
 
 router.use('/owner', ownerRoutes);
 router.use('/team', teamRoutes);
 router.use('/teamnames', teamNames);
+router.use('/ownerhomepage', teamsData);
 
 module.exports = router;
